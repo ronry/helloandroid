fd415beda091dcad31d8642c92382ab9b01dcee5
lvjinliang <jinliang.lvjl@alibaba-inc.com>
2015-01-07 21:28:54 +0800

    add simple button action

--- src/com/ronry/helloandroid/QuizActivity.java
+++ src/com/ronry/helloandroid/QuizActivity.java
@@ -4,13 +4,38 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends Activity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) this.findViewById(R.id.quiz_answer_true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this, "您的答案是“正确”", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mFalseButton = (Button) this.findViewById(R.id.quiz_answer_false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this, "您的答案是“错误”", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
