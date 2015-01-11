889d99ca7d059f5409234afff8a21d6cbfacabad
lvjinliang <jinliang.lvjl@alibaba-inc.com>
2015-01-11 17:40:59 +0800

    reactor: add SingleFragmentActivity

--- src/com/ronry/helloandroid/criminal/CriminalIntentActivity.java
+++ src/com/ronry/helloandroid/criminal/CriminalIntentActivity.java
@@ -1,27 +1,14 @@
 package com.ronry.helloandroid.criminal;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.os.Bundle;
 
-import com.ronry.helloandroid.R;
+import com.ronry.helloandroid.SingleFragmentActivity;
 
-public class CriminalIntentActivity extends Activity {
+public class CriminalIntentActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_criminal_intent);
-
-        FragmentManager fragmentManager = this.getFragmentManager();
-
-        Fragment criminalIntentFragment = fragmentManager.findFragmentById(R.id.criminal_intent_container);
-        if (criminalIntentFragment == null) {
-            criminalIntentFragment = new CriminalIntentFragment();
-            fragmentManager.beginTransaction().add(R.id.criminal_intent_container, criminalIntentFragment).commit();
-        }
+    protected Fragment createFragment() {
+        return new CriminalIntentFragment();
     }
 
 }
